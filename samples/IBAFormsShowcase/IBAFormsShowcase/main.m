6481d7cda684d66de7505b9ca4ec4ecf5c0b1ceb
Sean Woodhouse <sean@ittybittyapps.com>
2011-06-20 21:09:39 +1000

    Moved library header files into their own directory and reconfigured project so the header files no longer break archiving of the showcase app. Recreated and reconfigured showcase app from scratch.

---
+++ samples/IBAFormsShowcase/IBAFormsShowcase/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  IBAFormsShowcase
+//
+//  Created by Sean Woodhouse on 15/06/11.
+//  Copyright 2011 Itty Bitty Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"IBAFormsShowcaseAppDelegate");
+    [pool release];
+    return retVal;
+}
